3efe84d4e212e578dd5b49f564695e2fdcc7b336
Shubham <56202840+shub-dab@users.noreply.github.com>
2023-09-19 16:41:28 +0530

    Time: 6 ms (9.79%) | Memory: 41.3 MB (5.73%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,17 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        String ans = "";
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(c >= 65 && c <= 90) {
+                c += 32;
+            }
+
+            ans += c;
+        }
+
+        return ans;
     }
 }
